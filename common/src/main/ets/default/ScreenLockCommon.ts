1a79bba56fff766270adb1fd1da7e0883e4bdcc2
limengmeng <limengmeng41@huawei.com>
2022-05-25 10:58:03 +0800

    修改农历函数
    Signed-off-by: limengmeng <limengmeng41@huawei.com>

--- common/src/main/ets/default/ScreenLockCommon.ts
+++ common/src/main/ets/default/ScreenLockCommon.ts
@@ -14,173 +14,161 @@
  */
 import Log from './Log';
 import FileIo from '@ohos.fileio';
+import  { LunarCalendar }  from "./LunarCalendar"
 
 const TAG = 'ScreenLock-ScreenLockCommon';
 const DFAULT_SIZE = 4096;
 const CHAR_CODE_AT_INDEX = 0;
 
 export enum ScreenLockStatus {
-  Locking = 1,
-  Unlock = 2,
-  RecognizingFace = 3,
-  FaceNotRecognized = 4
+    Locking = 1,
+    Unlock = 2,
+    RecognizingFace = 3,
+    FaceNotRecognized = 4
 }
 
 export function ReadConfigFile(fileName) {
-  Log.showInfo(TAG, `readConfigFile fileName:${fileName}`);
-  try {
-    let stream = FileIo.createStreamSync(fileName, 'r');
-    Log.showInfo(TAG, `readConfigFile stream:` + stream);
-    let buf = new ArrayBuffer(DFAULT_SIZE);
-    let len = stream.readSync(buf);
-    Log.showInfo(TAG, `readConfigFile len:` + len);
-    let arr = new Uint8Array(buf);
-    let charAt = ' '.charCodeAt(CHAR_CODE_AT_INDEX);
-    for (let i = len;i < DFAULT_SIZE; i++) {
-      arr[i] = charAt;
-    }
-    let content = String.fromCharCode.apply(null, arr);
-    stream.closeSync();
-    Log.showInfo(TAG, `readConfigFile content:` + JSON.stringify(content));
-    return JSON.parse(content);
-  } catch (error) {
-    Log.showInfo(TAG, `readConfigFile error:` + JSON.stringify(error));
-  }
-}
-export function ConvertLunarCalendar(sy, sm, sd) {
-  let lunarYearArr = [
-    0x0b557,
-    0x06ca0, 0x0b550, 0x15355, 0x04da0, 0x0a5b0, 0x14573, 0x052b0, 0x0a9a8, 0x0e950, 0x06aa0,
-    0x0aea6, 0x0ab50, 0x04b60, 0x0aae4, 0x0a570, 0x05260, 0x0f263, 0x0d950, 0x05b57, 0x056a0,
-    0x096d0, 0x04dd5, 0x04ad0, 0x0a4d0, 0x0d4d4, 0x0d250, 0x0d558, 0x0b540, 0x0b6a0, 0x195a6,
-    0x095b0, 0x049b0, 0x0a974, 0x0a4b0, 0x0b27a, 0x06a50, 0x06d40, 0x0af46, 0x0ab60, 0x09570,
-    0x04af5, 0x04970, 0x064b0, 0x074a3, 0x0ea50, 0x06b58, 0x055c0, 0x0ab60, 0x096d5, 0x092e0,
-    0x0c960, 0x0d954, 0x0d4a0, 0x0da50, 0x07552, 0x056a0, 0x0abb7, 0x025d0, 0x092d0, 0x0cab5,
-    0x0a950, 0x0b4a0, 0x0baa4, 0x0ad50, 0x055d9, 0x04ba0, 0x0a5b0, 0x15176, 0x052b0, 0x0a930,
-    0x07954, 0x06aa0, 0x0ad50, 0x05b52, 0x04b60, 0x0a6e6, 0x0a4e0, 0x0d260, 0x0ea65, 0x0d530,
-    0x05aa0, 0x076a3, 0x096d0, 0x04afb, 0x04ad0, 0x0a4d0, 0x1d0b6, 0x0d250, 0x0d520, 0x0dd45,
-    0x0b5a0, 0x056d0, 0x055b2, 0x049b0, 0x0a577, 0x0a4b0, 0x0aa50, 0x1b255, 0x06d20, 0x0ada0,
-    0x14b63, 0x09370, 0x049f8, 0x04970, 0x064b0, 0x168a6, 0x0ea50, 0x06b20, 0x1a6c4, 0x0aae0,
-    0x0a2e0, 0x0d2e3, 0x0c960, 0x0d557, 0x0d4a0, 0x0da50, 0x05d55, 0x056a0, 0x0a6d0, 0x055d4,
-    0x052d0, 0x0a9b8, 0x0a950, 0x0b4a0, 0x0b6a6, 0x0ad50, 0x055a0, 0x0aba4, 0x0a5b0, 0x052b0,
-    0x0b273, 0x06930, 0x07337, 0x06aa0, 0x0ad50, 0x14b55, 0x04b60, 0x0a570, 0x054e4, 0x0d160,
-    0x0e968, 0x0d520, 0x0daa0, 0x16aa6, 0x056d0, 0x04ae0, 0x0a9d4, 0x0a2d0, 0x0d150, 0x0f252,
-    0x0d520
-  ],
-    lunarMonth = ['正', '二', '三', '四', '五', '六', '七', '八', '九', '十', '冬', '腊'],
-    lunarDay = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '初', '廿'],
-     HeavenlyStemsAnd = ['甲', '乙', '丙', '丁', '戊', '己', '庚', '辛', '壬', '癸'],
-    EarthlyBranches = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥'];
-
-  function ConvertLunarCalendar(sy, sm, sd) {
-    sm -= 1;
-    let daySpan = (Date.UTC(sy, sm, sd) - Date.UTC(1949, 0, 29)) / (24 * 60 * 60 * 1000) + 1;
-    let ly, lm, ld;
-    for (let j = 0; j < lunarYearArr.length; j++) {
-      daySpan -= lunarYearDays(lunarYearArr[j]);
-      if (daySpan <= 0) {
-        ly = 1949 + j;
-        daySpan += lunarYearDays(lunarYearArr[j]);
-        break
-      }
-    }
-
-    for (let k = 0; k < lunarYearMonths(lunarYearArr[ly - 1949]).length; k++) {
-      daySpan -= lunarYearMonths(lunarYearArr[ly - 1949])[k];
-      if (daySpan <= 0) {
-        if (hasLeapMonth(lunarYearArr[ly - 1949]) > -1 && hasLeapMonth(lunarYearArr[ly - 1949]) <= k) {
-          if (hasLeapMonth(lunarYearArr[ly - 1949]) < k) {
-            lm = k;
-          } else if (hasLeapMonth(lunarYearArr[ly - 1949]) === k) {
-            lm = '闰' + k;
-          } else {
-            lm = k + 1;
-          }
-        } else {
-          lm = k + 1;
+    Log.showInfo(TAG, `readConfigFile fileName:${fileName}`);
+    try {
+        let stream = FileIo.createStreamSync(fileName, 'r');
+        Log.showInfo(TAG, `readConfigFile stream:` + stream);
+        let buf = new ArrayBuffer(DFAULT_SIZE);
+        let len = stream.readSync(buf);
+        Log.showInfo(TAG, `readConfigFile len:` + len);
+        let arr = new Uint8Array(buf);
+        let charAt = ' '.charCodeAt(CHAR_CODE_AT_INDEX);
+        for (let i = len;i < DFAULT_SIZE; i++) {
+            arr[i] = charAt;
         }
-        daySpan += lunarYearMonths(lunarYearArr[ly - 1949])[k];
-        break
-      }
+        let content = String.fromCharCode.apply(null, arr);
+        stream.closeSync();
+        Log.showInfo(TAG, `readConfigFile content:` + JSON.stringify(content));
+        return JSON.parse(content);
+    } catch (error) {
+        Log.showInfo(TAG, `readConfigFile error:` + JSON.stringify(error));
     }
-    ld = daySpan;
-    if (hasLeapMonth(lunarYearArr[ly - 1949]) > -1 && (typeof (lm) === 'string' && lm.indexOf('闰') > -1)) {
-      let reg = /\d/.exec(lm)
-      lm = `闰${lunarMonth[Number(reg)- 1]}`
-    } else {
-      lm = lunarMonth[lm - 1];
-    }
-    ly = getHeavenlyStemsAnd(ly) + getEarthlyBranches(ly);
-    if (ld < 11) {
-      ld = `${lunarDay[10]}${lunarDay[ld-1]}`
-    } else if (ld > 10 && ld < 20) {
-      ld = `${lunarDay[9]}${lunarDay[ld-11]}`
-    } else if (ld === 20) {
-      ld = `${lunarDay[1]}${lunarDay[9]}`
-    } else if (ld > 20 && ld < 30) {
-      ld = `${lunarDay[11]}${lunarDay[ld-21]}`
-    } else if (ld === 30) {
-      ld = `${lunarDay[2]}${lunarDay[9]}`
-    }
-    return {
-      lunarYear: ly,
-      lunarMonth: lm,
-      lunarDay: ld,
-    }
-  }
+}
 
-  function hasLeapMonth(ly) {
-    if (ly & 0xf) {
-      return ly & 0xf
-    } else {
-      return -1
+export function ConvertLunarCalendar(GregorianCalendarYear, GregorianCalendarMonth, GregorianCalendarDay) {
+    let InitialLunarTime = 1949
+    let LastHexadecimalDigit = 0xf
+    let HexadecimalFirstDigit = 0xf0000
+    let ConvertToHexDigit = 0x8000
+    let ConvertToHex = 0x8
+    let lunarMonth = ['正', '二', '三', '四', '五', '六', '七', '八', '九', '十', '冬', '腊'],
+        lunarDay = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '初', '廿'],
+        HeavenlyStemsAnd = ['甲', '乙', '丙', '丁', '戊', '己', '庚', '辛', '壬', '癸'],
+        EarthlyBranches = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥'];
+
+    function ConvertLunarCalendar(GregorianCalendarYear, GregorianCalendarMonth, GregorianCalendarDay) {
+        GregorianCalendarMonth -= 1;
+        let daySpan = (Date.UTC(GregorianCalendarYear, GregorianCalendarMonth, GregorianCalendarDay) - Date.UTC(InitialLunarTime, 0, 29)) / (24 * 60 * 60 * 1000) + 1;
+        let OutputLunarYear, OutputLunarMonth, OutputLunarDay;
+        for (let j = 0; j < LunarCalendar.length; j++) {
+            daySpan -= lunarYearDays(LunarCalendar[j]);
+            if (daySpan <= 0) {
+                OutputLunarYear = InitialLunarTime + j;
+                daySpan += lunarYearDays(LunarCalendar[j]);
+                break
+            }
+        }
+
+        for (let k = 0; k < lunarYearMonths(LunarCalendar[OutputLunarYear - InitialLunarTime]).length; k++) {
+            daySpan -= lunarYearMonths(LunarCalendar[OutputLunarYear - InitialLunarTime])[k];
+            if (daySpan <= 0) {
+                if (hasLeapMonth(LunarCalendar[OutputLunarYear - InitialLunarTime]) > -1 && hasLeapMonth(LunarCalendar[OutputLunarYear - InitialLunarTime]) <= k) {
+                    if (hasLeapMonth(LunarCalendar[OutputLunarYear - InitialLunarTime]) < k) {
+                        OutputLunarMonth = k;
+                    } else if (hasLeapMonth(LunarCalendar[OutputLunarYear - InitialLunarTime]) === k) {
+                        OutputLunarMonth = '闰' + k;
+                    } else {
+                        OutputLunarMonth = k + 1;
+                    }
+                } else {
+                    OutputLunarMonth = k + 1;
+                }
+                daySpan += lunarYearMonths(LunarCalendar[OutputLunarYear - InitialLunarTime])[k];
+                break
+            }
+        }
+        OutputLunarDay = daySpan;
+        if (hasLeapMonth(LunarCalendar[OutputLunarYear - InitialLunarTime]) > -1 && (typeof (OutputLunarMonth) === 'string' && OutputLunarMonth.indexOf('闰') > -1)) {
+            let reg = /\d/.exec(OutputLunarMonth)
+            OutputLunarMonth = `闰${lunarMonth[Number(reg)- 1]}`
+        } else {
+            OutputLunarMonth = lunarMonth[OutputLunarMonth - 1];
+        }
+        OutputLunarYear = getHeavenlyStemsAnd(OutputLunarYear) + getEarthlyBranches(OutputLunarYear);
+        if (OutputLunarDay < 11) {
+            OutputLunarDay = `${lunarDay[10]}${lunarDay[OutputLunarDay-1]}`
+        } else if (OutputLunarDay > 10 && OutputLunarDay < 20) {
+            OutputLunarDay = `${lunarDay[9]}${lunarDay[OutputLunarDay-11]}`
+        } else if (OutputLunarDay === 20) {
+            OutputLunarDay = `${lunarDay[1]}${lunarDay[9]}`
+        } else if (OutputLunarDay > 20 && OutputLunarDay < 30) {
+            OutputLunarDay = `${lunarDay[11]}${lunarDay[OutputLunarDay-21]}`
+        } else if (OutputLunarDay === 30) {
+            OutputLunarDay = `${lunarDay[2]}${lunarDay[9]}`
+        }
+        return {
+            lunarYear: OutputLunarYear,
+            lunarMonth: OutputLunarMonth,
+            lunarDay: OutputLunarDay,
+        }
     }
-  }
 
-  function leapMonthDays(ly) {
-    if (hasLeapMonth(ly) > -1) {
-      return (ly & 0xf0000) ? 30 : 29
-    } else {
-      return 0
-    }
-  }
-
-  function lunarYearDays(ly) {
-    let totalDays = 0;
-    for (let i = 0x8000; i > 0x8; i >>= 1) {
-      let monthDays = (ly & i) ? 30 : 29;
-      totalDays += monthDays;
-    }
-    if (hasLeapMonth(ly) > -1) {
-      totalDays += leapMonthDays(ly);
+    function hasLeapMonth(OutputLunarYear) {
+        if (OutputLunarYear & LastHexadecimalDigit) {
+            return OutputLunarYear & LastHexadecimalDigit
+        } else {
+            return -1
+        }
     }
 
-    return totalDays
-  }
-
-  function lunarYearMonths(ly) {
-    let monthArr = [];
-    for (let i = 0x8000; i > 0x8; i >>= 1) {
-      monthArr.push((ly & i) ? 30 : 29);
-    }
-    if (hasLeapMonth(ly)) {
-      monthArr.splice(hasLeapMonth(ly), 0, leapMonthDays(ly));
+    function leapMonthDays(OutputLunarYear) {
+        if (hasLeapMonth(OutputLunarYear) > -1) {
+            return (OutputLunarYear & HexadecimalFirstDigit) ? 30 : 29
+        } else {
+            return 0
+        }
     }
 
-    return monthArr
-  }
+    function lunarYearDays(OutputLunarYear) {
+        let totalDays = 0;
+        for (let i = ConvertToHexDigit; i > ConvertToHex; i >>= 1) {
+            let monthDays = (OutputLunarYear & i) ? 30 : 29;
+            totalDays += monthDays;
+        }
+        if (hasLeapMonth(OutputLunarYear) > -1) {
+            totalDays += leapMonthDays(OutputLunarYear);
+        }
 
-  function getHeavenlyStemsAnd(ly) {
-    let HeavenlyStemsAndKey = (ly - 3) % 10;
-    if (HeavenlyStemsAndKey === 0) HeavenlyStemsAndKey = 10;
-    return HeavenlyStemsAnd[HeavenlyStemsAndKey - 1]
-  }
+        return totalDays
+    }
 
-  function getEarthlyBranches(ly) {
-    let EarthlyBranchesKey = (ly - 3) % 12;
-    if (EarthlyBranchesKey === 0) EarthlyBranchesKey = 12;
-    return EarthlyBranches[EarthlyBranchesKey - 1]
-  }
+    function lunarYearMonths(OutputLunarYear) {
+        let monthArr = [];
+        for (let i = ConvertToHexDigit; i > ConvertToHex; i >>= 1) {
+            monthArr.push((OutputLunarYear & i) ? 30 : 29);
+        }
+        if (hasLeapMonth(OutputLunarYear)) {
+            monthArr.splice(hasLeapMonth(OutputLunarYear), 0, leapMonthDays(OutputLunarYear));
+        }
 
-  return ConvertLunarCalendar(sy, sm, sd)
+        return monthArr
+    }
+
+    function getHeavenlyStemsAnd(OutputLunarYear) {
+        let HeavenlyStemsAndKey = (OutputLunarYear - 3) % 10;
+        if (HeavenlyStemsAndKey === 0) HeavenlyStemsAndKey = 10;
+        return HeavenlyStemsAnd[HeavenlyStemsAndKey - 1]
+    }
+
+    function getEarthlyBranches(OutputLunarYear) {
+        let EarthlyBranchesKey = (OutputLunarYear - 3) % 12;
+        if (EarthlyBranchesKey === 0) EarthlyBranchesKey = 12;
+        return EarthlyBranches[EarthlyBranchesKey - 1]
+    }
+
+    return ConvertLunarCalendar(GregorianCalendarYear, GregorianCalendarMonth, GregorianCalendarDay)
 }
