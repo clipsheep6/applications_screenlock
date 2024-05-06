bcf16bf3e81daf3cd96877a9dc0c77b167185fc1
zhengyongjie <15531316327@163.com>
2024-05-06 17:37:36 +0800

    定时器3S 
    Signed-off-by: zhengyongjie <15531316327@163.com>

--- features/screenlock/src/main/ets/com/ohos/model/screenLockService.ts
+++ features/screenlock/src/main/ets/com/ohos/model/screenLockService.ts
@@ -281,7 +281,7 @@ export class ScreenLockService {
                     })
                     this.isLoading = false;
                     Log.showError(TAG, `定时器执行完成`);
-                }, 2000);
+                }, 3000);
             } else {
                 return
             }
