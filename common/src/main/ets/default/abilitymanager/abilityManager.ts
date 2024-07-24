9abf563ffc5be1eb4abcce94975baca5e3c28ad2
18671941053 <zoujunjie6@huawei.com>
2024-07-24 17:19:06 +0800

    zjj0724
    Signed-off-by: 18671941053 <zoujunjie6@huawei.com>

--- common/src/main/ets/default/abilitymanager/abilityManager.ts
+++ common/src/main/ets/default/abilitymanager/abilityManager.ts
@@ -13,11 +13,15 @@
  * limitations under the License.
  */
 
+// 导入日志模块
 import Log from '../Log';
 
 const TAG = 'AbilityManager';
 
+// 导出一个默认类 AbilityManager，这个类包含了管理 abilities 的静态方法
 export default class AbilityManager {
+
+  // 定义了不同的 abilities 名称，每个名称对应一个系统 UI 组件
   static ABILITY_NAME_ENTRY = 'SystemUi_Entry';
   static ABILITY_NAME_STATUS_BAR = 'SystemUi_StatusBar';
   static ABILITY_NAME_NAVIGATION_BAR = 'SystemUi_NavigationBar';
@@ -29,11 +33,15 @@ export default class AbilityManager {
   static ABILITY_NAME_BANNER_NOTICE = 'SystemUi_BannerNotice';
   static ABILITY_NAME_SCREEN_LOCK = 'SystemUi_ScreenLock';
 
+  // 设置 abilities 上下文的静态方法，接受一个 ability 名称和一个上下文对象
+  // 将这个上下文存储在全局变量 globalThis 中
   static setContext(abilityName: string, context) {
     Log.showDebug(TAG, `setContext, abilityName: ${abilityName}`);
     globalThis[abilityName + '_Context'] = context;
   }
 
+  // 获取 abilities 上下文的静态方法，如果没有提供 ability 名称，默认使用 ABILITY_NAME_ENTRY
+  // getContext 方法用于获取存储在 globalThis 中的 abilities 上下文
   static getContext(abilityName?: string) {
     Log.showDebug(TAG, `getContext, abilityName: ${abilityName}`);
     if (!abilityName) {
@@ -42,16 +50,20 @@ export default class AbilityManager {
     return globalThis[abilityName + '_Context'];
   }
 
+  // 设置 abilities 数据的静态方法，接受 ability名称、键和数据，将数据存储在全局变量 globalThis 中
   static setAbilityData(abilityName, key, data) {
     Log.showDebug(TAG, `setAbilityData, abilityName: ${abilityName} key: ${key} data: ${JSON.stringify(data)}`);
     globalThis[abilityName + '_data_' + key] = data;
   }
 
+  // 获取 abilities 数据的静态方法，getAbilityData 方法用于从 globalThis 中获取存储的 abilities 数据
   static getAbilityData(abilityName, key) {
     Log.showDebug(TAG, `getAbilityData, abilityName: ${abilityName} key: ${key} `);
     return globalThis[abilityName + '_data_' + key];
   }
 
+  // 启动 abilities 的静态方法，接受一个 want 对象，描述要启动的 ability，以及一个可选的回调函数
+  // 使用 context.startAbility 启动 ability，并处理 Promise 的 .then 和 .catch
   static startAbility(want, callback?: Function) {
     Log.showDebug(TAG, `startAbility, want: ${JSON.stringify(want)}`);
     let context = AbilityManager.getContext();
